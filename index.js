d597a188c03bb8c28e3aff8861c58f6bd89431ac
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-04 12:35:06 +0200

    Enabled CORS for all domains

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'POST, PUT, GET');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
 app.get('/', function(req, res) {
+  res.json({ success: true })
+})
+
+app.post('/', function(req, res) {
   res.json({ success: true })
 })
 
