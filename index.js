3169ad801a479322fcf63f3c01702f4a46250190
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-04 14:30:42 +0200

    (chore) Added pg - postgres bindings for node

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
+app.set('databaseString', process.env.DATABASE_URL)
+
+var pg = require('pg');
+var conString = app.get('databaseString');
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
 
 app.all('*', function(req, res, next){
   if (!req.get('Origin')) return next();
