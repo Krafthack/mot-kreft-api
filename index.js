5b53a959416e7ef38daca3b593780329fad4deb5
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-11 11:35:11 +0200

    (refac) Moved the index endpoint into api/index

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var configure = require('./src/config/app-config');
-
 var api = require('./src/api');
 var dummies = require('./src/mocks/dummy');
 var pg = require('./src/lib/q-pg');
 var app = express();
 
 configure(app);
-
 app.use(api);
-
-app.get('/', function(req, res) {
-  res.json({ success: true, message: 'Welcome'})
-});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,9 @@ var app = express();
 app.use(moodsApi);
 app.use(usersApi);
 
+app.get('/', function(req, res) {
+  res.json({ success: true, message: 'Welcome'})
+});
+
+
 module.exports = app;
