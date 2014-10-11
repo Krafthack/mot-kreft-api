34305d45fdcc20479cc241e5f2aabda2f3c9e911
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-11 11:34:20 +0200

    (refac) Moved middleware setup into app-config

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
 var configure = require('./src/config/app-config');
-var enableCors = require('./src/middlewares/enable-cors');
+
 var api = require('./src/api');
 var dummies = require('./src/mocks/dummy');
 var pg = require('./src/lib/q-pg');
 var app = express();
 
 configure(app);
-
-app.use(bodyParser.json());
-app.use(session( {
-  secret: app.get('sessionKey')
-}));
-
-app.all('*', enableCors);
 
 app.use(api);
 
--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,6 +1,12 @@
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var enableCors = require('../middlewares/enable-cors');
 
 module.exports = function(app) {
   app.set('port', (process.env.PORT || 5000))
   app.set('databaseString', process.env.DATABASE_URL)
   app.set('sessionKey', process.env.SESSION_KEY || '1234567890 dev key');
+  app.use(bodyParser.json());
+  app.use(session( { secret: app.get('sessionKey') }));
+  app.all('*', enableCors);
 }
