0fa0e4a5201d4ae77f9c7382fb013da332c545cd
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-16 10:58:13 +0200

    (chore) Forgot a few datamodels

--- db/model.sql
+++ db/model.sql
@@ -1,9 +1,16 @@
 drop table hugs;
 drop table days;
 drop table moods;
+drop table caretaker;
 drop table users;
 
 create table users (
+  id serial,
+  name varchar(50),
+  primary key (id)
+);
+
+create table caretaker (
   id serial,
   name varchar(50),
   primary key (id)
@@ -15,6 +22,7 @@ create table moods (
   ts timestamp,
   comment text,
   location text,
+  feel integer,
   primary key (id),
   foreign key (user_id) references users(id)
 );
