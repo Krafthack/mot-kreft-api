15e5ff9e5f95c3a6136e5f486ac370c4bc44a261
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-17 08:43:32 +0200

    (refac) Split the query into two. Is this better than before?

--- src/api/days.js
+++ src/api/days.js
@@ -1,4 +1,7 @@
 var pg = require('pg');
+var Q = require('q');
+var _ = require('lodash');
+var moment = require('moment');
 var express = require('express');
 var app = express();
 
@@ -11,38 +14,48 @@ app.get('/days', function(req, res) {
     }
     var userId = req.session.user.id;
       client.query(
-        'select days.id as days_id, days.user_id, day, day_type, ts as hugs_ts, from_name as hug_from from days left outer join hugs on (hugs.ts < days.day and (days.day-1) < hugs.ts) where days.user_id = $1::int',
+        'select * from days where user_id = $1::int',
         [userId] ,function(err, result) {
-      done();
 
       if(err) {
         return res.status(500).json( {success: false, error: err});
       }
 
-      var group = result.rows.reduce(function(pre, cur) {
-        var key = cur.day;
-        if (pre[key] == null) { pre[key] = []; }
-        pre[key].push(cur)
-        return pre;
-      }, {})
-
-      var data = Object.keys(group).map(function(key) {
-        var hugs = group[key].filter(function(hug) {
-          return hug.hugs_ts != null;
-        }).map(function(hug) {
-          return {
-            hugs_ts: hug.hugs_ts,
-            hg_from: hug.hug_from
+      var hugs = result.rows.map(function(day) {
+        var deferred = Q.defer();
+        var today = moment(day.day).format('YYYY-MM-DD');
+        var yester = moment(today).add(-1, 'day').format('YYYY-MM-DD');
+        client.query('select * from hugs where hugs.ts < $1 and hugs.ts > $2 and user_id = $3',
+        [today, yester, userId], function(err, result) {
+          if (err) {
+            return deferred.reject(err);
+          } else {
+            return deferred.resolve(result.rows);
           }
-        });
-        return {
-          day: group[key][0].day,
-          days_id: group[key][0].days_id,
-          hugs: hugs
-        }
-      });
+        })
+        return deferred.promise;
+      })
 
-      return res.json({ success: true, data: data })
+      Q.allSettled(hugs).then(function(results) {
+        done();
+        var hugs = results.map(function(result) {
+          return result.value;
+        });
+
+        var data  = _(result.rows).zip(hugs)
+        .map(function(result) {
+          var day = result[0];
+          day.hugs = result[1];
+          return day;
+        })
+        .value()
+
+
+        return res.json({ success: true, data: data })
+      }, function() {
+        console.log('something went horribly');
+      })
+
     });
   });
 });
